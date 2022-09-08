3bc131c750a3b545425980086bf8c85c4ecfd0ad
Jignesh Bhatt <j.sbhatt09@gmail.com>
2022-09-08 16:58:44 +0200

    Added graphics into the game

--- code/level.py
+++ code/level.py
@@ -1,10 +1,13 @@
 from numpy import tile
 import pygame
+from support import import_folder
 
 from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import import_csv_layout
+from random import choice
 
 
 class Level:
@@ -21,16 +24,37 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        # converted worldmap into positions
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player(
-                        (x, y), [self.visible_sprites], self.obstacle_sprites)
+        layouts = {
+            'boundary': import_csv_layout('E:/PythonNew/Game/PYTHON_GAME_ZELDA/graphics/map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('E:/PythonNew/Game/PYTHON_GAME_ZELDA/graphics/map/map_Grass.csv'),
+            'object': import_csv_layout('E:/PythonNew/Game/PYTHON_GAME_ZELDA/graphics/map/map_Objects.csv')
+        }
+        graphics = {
+            'grass': import_folder('E:/PythonNew/Game/PYTHON_GAME_ZELDA/graphics/grass'),
+            'objects' : import_folder('E:/PythonNew/Game/PYTHON_GAME_ZELDA/graphics/objects')
+        }
+
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == "boundary":
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
+                        if style == "grass":
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x,y), [self.visible_sprites,self.obstacle_sprites],'grass',random_grass_image)
+                        if style == "object":
+                            surf = graphics['objects'][int(col)]
+                            Tile((x,y), [self.visible_sprites,self.obstacle_sprites],'object',surf)
+
+                    # if col == 'x':
+                    #     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                    # if col == 'p':
+
+        self.player = Player(
+            (2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
@@ -47,11 +71,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1]//2
         self.offset = pygame.math.Vector2()
 
+        # Creating the floor
+        self.floor_surf = pygame.image.load(
+            "E:/PythonNew/Game/PYTHON_GAME_ZELDA/graphics/tilemap/ground.png").convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
+
     def custom_draw(self, player):
         # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+        # drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
